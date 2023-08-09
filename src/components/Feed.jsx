f3d114c612303dbbb9257d9e7649dcab22ac99ea
“Abhishek <abhishek.shyam@brightchamps.com>
2023-08-09 12:45:13 +0530

    changes

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Stack, Box, Typography } from "@mui/material";
+import SideBar from "./SideBar";
+import Videos from "./Videos";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [selectedCategory, setselectedCategory] = useState("New");
+  const [Videos, setVidoes] = useState("");
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`, [
+      selectedCategory,
+    ]).then((data) => setVidoes(data.items));
+    console.log(Videos);
+  });
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid white",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <SideBar
+          selectedCategory={selectedCategory}
+          setselectedCategory={setselectedCategory}
+        />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "white" }}
+        >
+          copyright 2022 JSM media
+        </Typography>
+      </Box>
+      <Box sx={{ p: 2, height: "90vh", overflow: "auto" }}>
+        <Typography variant="h4" sx={{ color: "white", mb: 2 }}>
+          {selectedCategory} <span style={{ color: "red" }}>Videos</span>
+        </Typography>
+      </Box>
+    </Stack>
+  );
 };
 
 export default Feed;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Paper, IconButton } from "@mui/material";
+import { Search } from "@mui/icons-material";
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  return (
+    <Paper
+      component="form"
+      onSubmit={() => {}}
+      sx={{
+        borderRadius: 20,
+        border: "1px solid #e3e3e3",
+        pl: 2,
+        mr: { sm: 5 },
+      }}
+    >
+      <input
+        placeholder="Search..."
+        className="search-bar"
+        onChange={() => {}}
+      />
+      <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
+        <Search />
+      </IconButton>
+    </Paper>
+  );
 };
 
 export default SearchBar;

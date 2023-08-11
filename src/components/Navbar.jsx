45d7abab4fe71147d83996f7843c0b5b8e53a944
“Abhishek <abhishek.shyam@brightchamps.com>
2023-08-11 16:36:53 +0530

    video_components_done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,19 +4,20 @@ import { Stack } from "@mui/material";
 
 import { logo } from "../utils/constants";
 import SearchBar from "./SearchBar";
+
 const Navbar = () => (
   <Stack
     direction="row"
     alignItems="center"
     p={2}
     sx={{
-      backgroundColor: "black",
-      justifyContent: "space-between",
       position: "sticky",
+      background: "#000",
       top: 0,
+      justifyContent: "space-between",
     }}
   >
-    <Link path="/" style={{ alignItems: "center", display: "flex" }}>
+    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="logo" height={45} />
     </Link>
     <SearchBar />
